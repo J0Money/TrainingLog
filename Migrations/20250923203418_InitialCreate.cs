50d920c8d00590bf0d49292b596ad496b2e9dba7
J0Money <joachimh270@gmail.com>
2025-09-24 07:30:57 +0200

    Initial commit

---
+++ Migrations/20250923203418_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingLog.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workouts");
+        }
+    }
+}
---
+++ Models/Workout.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainingLog.Models;
+
+public class Workout
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Type { get; set; }
+    public string? Notes { get; set; }
+}
